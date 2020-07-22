7e748aad72abe6e6e4e1a04d6d66e4301e09907e
Jess Yoder <jessica.m.yoder@gmail.com>
2020-07-22 14:38:24 -0400

    Added api_key and g_key to config files

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ee54d0864c57cc1ac4dea26eaf1e16b8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAKln95pBQB3GjiODnw-PRW6sqZy_te_VQ"
